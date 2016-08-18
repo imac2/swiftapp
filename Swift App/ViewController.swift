058d086a727e3cbd1709ae4d4fb80f4ee9dfbb5d
Ima Christian <imachristian@Imas-MBP-2.attlocal.net>
2016-08-18 15:50:08 -0400

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Ima Christian on 8/18/16.
+//  Copyright (c) 2016 Pinetart Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
